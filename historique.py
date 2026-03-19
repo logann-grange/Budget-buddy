1e27e59a1a40879940402680fea82745ae0d0939
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-19 17:53:31 +0100

    recuperation de la logique de l'historique

--- historique.py
+++ historique.py
@@ -1,44 +1,26 @@
-import customtkinter as ctk
+from transaction import Transaction
 
-class Historique:
-    def __init__(self, display):
-        self.display = display
-        self.frame = None
+class Historique : 
 
-    def afficher(self):
-        self.frame = ctk.CTkFrame(self.display, width=1080, height=720, fg_color="transparent")
-        self.frame.place(x=0, y=0)
+    def __init__(self, login_id, transactions):
+        self.transactions = transactions
+        self.filter = ["", "", ""] #type, montant, date
+        displayed_transaction = []
+        self.display_transaction()
 
-        back_button = ctk.CTkButton(self.frame, text="← Retour", command=self.retour_menu, fg_color="red", hover_color="#FD4F4F", width=100, height=30)
-        back_button.place(x=20, y=20)
+    def get_to_string(self) :
+        str_historique = ""
+        for transaction in self.transactions :
+            str_historique += transaction.description + ";"
+        return str_historique
 
-        title_label = ctk.CTkLabel(self.frame, text="Historique détaillé", font=("Helvetica", 32, "bold"))
-        title_label.place(x=400, y=30)
+    def display_transaction(self) :
+        list_transaction = []
+        for transaction in self.transactions :
+            if transaction.type == (self.filter[0] in transaction.type or self.filter[0] == "")  and (self.filter[1] == transaction.montant or self.filter[1] == "") and (self.filter[2] in transaction.date or self.filter[2]=="") :
+                list_transaction.append(transaction)
 
-        depenses_frame = ctk.CTkFrame(self.frame, width=500, height=600, fg_color="white", border_width=2, border_color="white")
-        depenses_frame.place(x=20, y=100)
+        self.displayed_transaction = list_transaction
+                
+                
 
-        depenses_label = ctk.CTkLabel(depenses_frame, text="de -X€ date raison", font=("Helvetica", 18, "bold"), text_color="red")
-        depenses_label.place(x=20, y=20)
-
-        depenses_text = ctk.CTkLabel(depenses_frame,
-            text="",
-            font=("Helvetica", 12), text_color="black", justify="left")
-        depenses_text.place(x=20, y=70)
-
-        revenus_frame = ctk.CTkFrame(self.frame, width=500, height=600, fg_color="white", border_width=2, border_color="white")
-        revenus_frame.place(x=540, y=100)
-
-        revenus_label = ctk.CTkLabel(revenus_frame, text="a +X€ date", font=("Helvetica", 18, "bold"), text_color="green")
-        revenus_label.place(x=20, y=20)
-
-        revenus_text = ctk.CTkLabel(revenus_frame,
-            text="",
-            font=("Helvetica", 12), text_color="black", justify="left")
-        revenus_text.place(x=20, y=70)
-
-    def retour_menu(self):
-        from menu import Menu
-        self.frame.destroy()
-        menu = Menu(self.display)
-        menu.afficher()
